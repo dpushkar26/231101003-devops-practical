bf64b0e553d4d91b079d97a5dea1bac99f1383c5
Pushkar Deore <deorepushkar2305@gmail.com>
2026-06-02 10:18:18 +0530

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
